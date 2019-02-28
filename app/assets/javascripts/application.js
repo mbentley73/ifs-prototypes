3178776f4abd72478888ef35fba7199cdad41869
Matthew Bentley <mbentley@worth.systems>
2019-02-28 16:59:34 +0000

    Add nav to index page, add Aceesible autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,11 @@
 /* global $ */
 
+const countries = [
+  'France',
+  'Germany',
+  'United Kingdom'
+]
+
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
@@ -7,4 +13,13 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#location-picker')
+  })
+  accessibleAutocomplete({
+    element: document.querySelector('#location-picker'),
+    id: 'location-picker', // To match it to the existing <label>.
+    source: countries
+  })
 })
